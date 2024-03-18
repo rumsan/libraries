8a5699ebaaca890c23a5256c1a2eb93f270b2dad
Raghav Kattel <arg.ahv7@gmail.com>
2024-03-18 22:15:40 +0545

    Refactor auth.query.ts

--- libs/react-query/src/auth/auth.query.ts
+++ libs/react-query/src/auth/auth.query.ts
@@ -4,41 +4,38 @@ import { useErrorStore } from '../utils';
 import { useAuthStore } from './auth.store';
 
 
-export const   useRequestOtp= ()=> {
-    const onError = useErrorStore((state) => state.setError);
+export const useRequestOtp = () => {
+  const onError = useErrorStore((state) => state.setError);
   const setChallenge = useAuthStore((state) => state.setChallenge);
+  const { queryClient, rumsanService } = useRSQuery();
+
+  return useMutation(
+    {
+      mutationFn: rumsanService.auth.getOtp,
+      onSuccess: (data) => {
+        setChallenge(data?.data.challenge);
+        return data.data;
+      },
+      onError,
+    },
+    queryClient
+  );
+};
+
+export const useVerifyOtp = () => {
+  const onError = useErrorStore((state) => state.setError);
+  const setToken = useAuthStore((state) => state.setToken);
   const { queryClient, rumsanService } = useRSQuery()
 
-  console.log('rumsanService', rumsanService)
-
-    return useMutation(
-      {
-        mutationFn: rumsanService.auth.getOtp,
-        onSuccess: (data) => {
-          setChallenge(data?.data.challenge);
-          return data.data;
-        },
-        onError,
+  return useMutation(
+    {
+      mutationFn: rumsanService.auth.login,
+      onSuccess: (data) => {
+        setToken(data?.data.accessToken);
+        return data.data;
       },
+      onError,
+    },
     queryClient
-    );
-  }
-
-  export const useVerifyOtp=()=> {
-    const onError = useErrorStore((state) => state.setError);
-    const setToken = useAuthStore((state) => state.setToken);
-  const {queryClient,rumsanService} = useRSQuery()
-
-
-    return useMutation(
-      {
-        mutationFn: rumsanService.auth.login,
-        onSuccess: (data) => {
-          setToken(data?.data.accessToken);
-          return data.data;
-        },
-        onError,
-      },
-     queryClient
-    );
+  );
 }
