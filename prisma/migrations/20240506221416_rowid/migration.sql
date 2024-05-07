a0b41d358c8a0613415e68ef172a74a26f2c9ee2
Raghav Kattel <arg.ahv7@gmail.com>
2024-05-07 20:36:47 +0545

    chore: Remove unused files and update dependencies

--- prisma/migrations/20240506221416_rowid/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `rowId` to the `tbl_audits` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "tbl_audits" ADD COLUMN     "rowId" INTEGER NOT NULL;
