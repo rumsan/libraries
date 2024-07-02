9a827dc3cf6cda1eb966a7e67402fc66983ef503
Binod Chaudhary <thebinod7@gmail.com>
2024-07-02 14:18:32 +0545

    clear console log

--- libs/user/src/lib/auths/auths.service.ts
+++ libs/user/src/lib/auths/auths.service.ts
@@ -52,7 +52,6 @@ export class AuthsService {
         },
       },
     });
-    console.log('DTO=>', dto);
     if (!auth) throw new ForbiddenException(`Invalid ${dto.service}!`);
     const otp = Math.floor(100000 + Math.random() * 900000);
     await this.prisma.auth.update({
