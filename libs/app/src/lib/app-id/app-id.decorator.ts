9e110a046220c5387f65e6c08d039015d7af4e45
myanzik <myanz.470shrestha@gmail.com>
2025-03-28 04:03:51 +0545

    updated rumsan/app

--- libs/app/src/lib/app-id/app-id.decorator.ts
+++ libs/app/src/lib/app-id/app-id.decorator.ts
@@ -2,7 +2,13 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const AppId = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.headers['app-id'];
+    if (ctx.getType() === 'http') {
+      const request = ctx.switchToHttp().getRequest();
+      return request.headers['app-id'];
+    } else if (ctx.getType() === 'rpc') {
+      const data = ctx.switchToRpc().getData();
+      return data['app-id'];
+    }
+    return null;
   },
 );
--- libs/app/src/lib/app-id/app-id.interceptor.ts
+++ libs/app/src/lib/app-id/app-id.interceptor.ts
@@ -10,31 +10,39 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class AppIdInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // Check if the context is HTTP-based to extract the appId
     if (context.getType() === 'http') {
-      const request = context.switchToHttp().getRequest();
-      const appId = request.headers['app-id'];
-
-      if (appId) {
-        request['appId'] = appId;
-      }
-    }
-    // Check if the context is message-based (e.g., Redis, Kafka)
-    if (context.getType() === 'rpc') {
-      const data = context.switchToRpc().getData();
-      const appId = data?.['app-id'];
-
-      if (appId) {
-        data['appId'] = appId;
-      }
+      this.handleHttpRequest(context);
+    } else if (context.getType() === 'rpc') {
+      this.handleRpcRequest(context);
     }
 
-    // Pass the request/message to 
-    // the next handler
     return next.handle().pipe(
       tap(() => {
         // Optionally, add additional logic here (e.g., logging)
       }),
     );
+  }
+
+  private handleHttpRequest(context: ExecutionContext): void {
+    const request = context.switchToHttp().getRequest();
+    const appId = request.headers['app-id'];
+
+    if (appId) {
+      request['appId'] = appId;
+    }
+  }
+
+  private handleRpcRequest(context: ExecutionContext): void {
+    const data = context.switchToRpc().getData();
+
+    const appId = data?.['app-id'];
+    if (appId) {
+      data['appId'] = appId;
+    }
+
+    const pattern = context.getHandler()?.name;
+    if (pattern) {
+      data['messagePattern'] = pattern;
+    }
   }
 }
